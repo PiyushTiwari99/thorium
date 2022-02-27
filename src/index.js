0d039f250b28b46ab3d4dd23e5b66e0c49363094
PiyushTiwari99 <piyushtiwarii142@gmail.com>
2022-02-27 23:14:20 +0530

    Done with my work

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://komalbansod_04:zjmLTAkg7aF42plB@komal04.fvnel.mongodb.net/komalbansod", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
 app.use('/', route);
 
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,29 +1,53 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
+//const userSchema = new mongoose.Schema({
+// firstName: String,
+// lastName: String,
+// mobile: {
+//     type: String,
+//     unique: true,
+//     required: true
+// },
+// emailId: String,
+// gender: {
+//     type: String,
+//     enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+// },
+// age: Number,
+// // isIndian: Boolean,
+// // parentsInfo: {
+// //     motherName: String,
+// //     fatherName: String,
+// //     siblingName: String
+// // },
+// // cars: [ String  ]
+
+// Create a bookSchema with bookName, authorName, category and year .Create same 2 api's for books i.e. : 1 api to create a new book and another api to get the list of all books. 
+
+
+
+
+const bookSchema = new mongoose.Schema({
+    bookName: {
+        type: String,
+        unique: true
+    },
+    authorName: {
+        type: String,
+        required: true
+    },
+    bookCategory: {
         type: String,
         unique: true,
         required: true
     },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    publishYear: Number
+    // bookList: [{ type: String }]
+
+
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('Piyush', bookSchema) //users
 
 
 
