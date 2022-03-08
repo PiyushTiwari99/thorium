aece47236ba0eff2294e087f51b08548ac6816e4
PiyushTiwari99 <piyushtiwarii142@gmail.com>
2022-03-09 03:02:16 +0530

    done work

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://komalbansod_04:zjmLTAkg7aF42plB@komal04.fvnel.mongodb.net/komalbansod?authSource=admin&replicaSet=atlas-jlotsj-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
 
 app.use('/', route)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,31 @@
+// const express = require('express');
+// const router = express.Router();
+// const userController = require("../controllers/userController")
+
+// router.get("/test-me", function (req, res) {
+//     res.send("My first ever api!")
+// })
+
+// router.post("/users", userController.createUser)
+
+// router.post("/login", userController.loginUser)
+
+// // //The userId is sent by front end
+// router.get("/users/:userId", userController.getUserData)
+
+// router.put("/users/:userId", userController.updateUser)
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
-const userController= require("../controllers/userController")
+const userController = require("../controllers/userController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser  )
+router.post("/createUser", userController.createUser)
 
 router.post("/login", userController.loginUser)
 
@@ -14,5 +33,8 @@ router.post("/login", userController.loginUser)
 router.get("/users/:userId", userController.getUserData)
 
 router.put("/users/:userId", userController.updateUser)
+
+router.delete("/users/:userId", userController.deleteUser)
+
 
 module.exports = router;
